e9bf1f68edc9b05ea089af5a7177bee5471451b3
chaitraliagashe <chaitraliagashe@gmail.com>
2024-03-24 19:50:32 +0100

    added pagination support for getPopularBlogs

--- src/main/java/com/shown/engagement/repository/UserEngagementRepository.java
+++ src/main/java/com/shown/engagement/repository/UserEngagementRepository.java
@@ -2,13 +2,13 @@ package com.shown.engagement.repository;
 
 import java.util.List;
 
-import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.shown.engagement.model.EngagementParameter;
 
 public interface UserEngagementRepository extends CrudRepository<EngagementParameter, String> {
 
-	List<EngagementParameter> findByOrderByLikesDesc(Limit of);
+	List<EngagementParameter> findByOrderByLikesDesc(Pageable paging);
 	
 }
